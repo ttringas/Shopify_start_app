d5e49ba469311a9a9e95bd745284d858242bb91d
Tyler Tringas <ttringas@gmail.com>
2012-12-04 14:33:30 -0600

    color picker

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :shopify, 
+       ShopifyApp.configuration.api_key, 
+       ShopifyApp.configuration.secret,
+       :scope => "write_products,read_orders",
+       :setup => lambda {|env| 
+                   params = Rack::Utils.parse_query(env['QUERY_STRING'])
+                   site_url = "https://#{params['shop']}"
+                   env['omniauth.strategy'].options[:client_options][:site] = site_url
+                 }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopifyAppTest2::Application.config.secret_token = 'd021c31ad1d8532bf985cfc9846c6866ffb259b2675337e5c23cf49a389f9a917cf67d53ee8941f5b220a3f2adb7dce80658351797dff75cf24fc5068a6eb5c3'
